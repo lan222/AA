77a995569f8f39344676e50a6a31ca73d482d832
aa <1123022212@qq.com>
2018-04-02 20:45:10 +0800

    Signed-off-by: aa <1123022212@qq.com>

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="lunbo">
+      <div class="banner swiper-container" id="m_home_banner">
+			<ul class="swiper-wrapper" id="banner">
+				<li class="swiper-slide"><a href="#"><img src="../images/2.03.jpg"/></a></li>
+				<li class="swiper-slide"><a href="#"><img src="../images/2.04.jpg"/></a></li>
+				<li class="swiper-slide"><a href="#"><img src="../images/2.05.jpg"/></a></li>
+				<li class="swiper-slide"><a href="#"><img src="../images/2.03.jpg"/></a></li>
+			</ul>
+			<div class="swiper-pagination banner-pagination"></div>	
+	    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+	var banner=new Swiper(".banner",{
+		speed:400,
+		autoplay:5000,
+		autoplaydisableoninteraction:false,
+		loop:true,
+		pagination:'.banner-pagination',
+		paginationClickable:true
+	 })
+	</script>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+
+<style>
+.lunbo{width: 90%;margin: 0 auto;}
+.lunbo img{width:100%}
+</style> 
+
